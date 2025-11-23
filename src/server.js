c65a552c4d7ef9d46168b91419931bccc5011c53
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-11-23 19:16:29 +0000

    add env

--- src/server.js
+++ src/server.js
@@ -1,81 +1,71 @@
-import express, { Router } from "express"
-import cors from "cors"
-import helmet from "helmet"
-import { connectMongoDB } from "./db/connectMongoDB.js"
-import { logger } from "./middleware/logger.js"
-import cookieParser from "cookie-parser"
-import { myCookieParser } from "./middleware/myCookieParser.js"
-import { notFoundHandler } from "./middleware/notFoundHandler.js"
-import { errors } from "celebrate"
-import { errorHandler } from "./middleware/errorHandler.js"
-import "dotenv/config"
-import authRoutes from "./routes/authRoutes.js"
-import goodsRoutes from "./routes/goodsRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-import ordersRoutes from "./routes/ordersRoutes.js"
-import swaggerJsdoc from "swagger-jsdoc"
-import swaggerUi from "swagger-ui-express"
+import express, { Router } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import cookieParser from 'cookie-parser';
+import { myCookieParser } from './middleware/myCookieParser.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errors } from 'celebrate';
+import { errorHandler } from './middleware/errorHandler.js';
+import 'dotenv/config';
+import authRoutes from './routes/authRoutes.js';
+import goodsRoutes from './routes/goodsRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import ordersRoutes from './routes/ordersRoutes.js';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
-const app = express()
-const PORT = process.env.PORT ?? 3030
-const router = Router()
+const app = express();
+const PORT = process.env.PORT ?? 3030;
+const router = Router();
 const specs = swaggerJsdoc({
-	definition: {
-		openapi: "3.0.0",
-		info: {
-			title: "Clothica API",
-			version: "1.0.0",
-			description: "Clothica API",
-		},
-	},
-	apis: ["./src/routes/*.js"],
-})
-const blackList = ["http://something.com"]
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs))
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Clothica API',
+      version: '1.0.0',
+      description: 'Clothica API',
+    },
+  },
+  apis: ['./src/routes/*.js'],
+});
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use(
-	express.json({
-		type: ["application/json", "application/vnd.api+json"],
-		limit: "100kb",
-	})
-)
-app.use(
-	cors({
-		origin: function (origin, callback) {
-			if (blackList.indexOf(origin) === -1) {
-				callback(null, true)
-			} else {
-				callback(new Error("Not allowed by CORS"))
-			}
-		},
-		credentials: true,
-	})
-)
+  express.json({
+    type: ['application/json', 'application/vnd.api+json'],
+    limit: '100kb',
+  }),
+);
+app.use(cors());
+
 app.use((req, res, next) => {
-	const IP = (req.headers["x-forwarded-for"] || req.socket.remoteAddress || "")
-		.split(",")[0]
-		.trim()
-	console.log("Request from: ", IP)
-	if (![].includes(IP)) {
-		next()
-	}
-})
-app.use(helmet())
-app.use(logger)
-app.use(cookieParser())
-app.use(myCookieParser)
+  const IP = (req.headers['x-forwarded-for'] || req.socket.remoteAddress || '')
+    .split(',')[0]
+    .trim();
+  console.log('Request from: ', IP);
+  if (![].includes(IP)) {
+    next();
+  }
+});
+app.use(helmet());
+app.use(logger);
+app.use(cookieParser());
+app.use(myCookieParser);
 
-app.use("/api", router)
-router.use(authRoutes)
-router.use(ordersRoutes)
-router.use(goodsRoutes)
-router.use(userRoutes)
+app.use('/api', router);
+router.use(authRoutes);
+router.use(ordersRoutes);
+router.use(goodsRoutes);
+router.use(userRoutes);
 
-app.use(notFoundHandler)
-app.use(errors())
-app.use(errorHandler)
+app.use(notFoundHandler);
+app.use(errors());
+app.use(errorHandler);
 
-await connectMongoDB()
+await connectMongoDB();
 
 app.listen(PORT, () => {
-	console.log(`✅ Server ran successfully on port ${PORT}`)
-})
+  console.log(`✅ Server ran successfully on port ${PORT}`);
+});
